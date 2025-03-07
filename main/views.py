b5da2d4879ed7d103dff315bdf9df06e087c83ca
Kot <131509642+bomba777201@users.noreply.github.com>
2025-03-07 19:25:36 +0300

    1

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import path
-
+from 123.py import login, password
 def main_page(request):
-    return render(request, 'main.html')
+    return render(request, 'main.html', context={'login': login, 'password': password})
+
 
